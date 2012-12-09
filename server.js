a44f9448a832af4871d6fd76530be208b57a084b
Jorgen Evens <jorgen@evens.eu>
2012-12-09 16:13:14 +0100

    Added MySQL database backend.

--- server.js
+++ server.js
@@ -1,34 +1,44 @@
 #!/usr/bin/env node
 
 var dns = require( 'node-dns' ),
+	RecordStore = require('./lib/RecordStore.js'),
+
+	test = (function(){
+		console.log( RecordStore );
+	})(),
 
 	server = dns.createServer(),
 
 	my_domain = 'dyn.evens.eu',
 
-	records = {
-		status: dns.A({
-			name: 'status',
-			address: '159.253.3.124',
-			ttl: 300
-		})
-	};
+	records = RecordStore.open( 'mysql-store', 'mysql', 'mysql://dev:dev@localhost/dyndns' );
+	records.addRecord( 'dyn.evens.eu', 'status', 'A', '159.253.3.124' );
 
 server.on( 'request', function( request, response ) {
-	var name = request.question[0].name.replace( '.' + my_domain, '' );
-	console.log( 'Request for: ', name );
+	var question = request.question[0],
+		qtype = dns.consts.QTYPE_TO_NAME[question.type],
+		name = question.name.replace( '.' + my_domain, '' );
 
-	if( !records[name] ) {
-		response.rcode = 3;
-		return response.send();
-	}
+	records.findRecord( my_domain, name, qtype, function( record ){
+		if( !record ) {
+			response.rcode = 3;
+			return response.send();
+		}
 
-	response.answer.push( records[name] );
-	response.send();
+		response.answer.push( dns[record.type]({
+			name: name + '.' + my_domain,
+			address: record.data.toString(),
+			data: this.address,
+			ttl: 300
+		}));
+
+		response.send();
+	} );
+	
 } );
 
 server.serve( 1053 );
 
 // TODO: Write a REST API to add / delete
-// TODO: Write records to DB, flush DB to disk
+// DONE: Write records to DB, flush DB to disk
 // TODO: Authentication ( per record key? )
