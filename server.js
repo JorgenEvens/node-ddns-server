d230df6da0ecf69e30de3903c18545a6868e72bc
Jorgen Evens <jorgen@evens.eu>
2012-12-16 20:35:28 +0100

    Fixed bug where MySQL connection would close.

--- server.js
+++ server.js
@@ -3,20 +3,8 @@
 var dns = require( 'node-dns' ),
 	RecordStore = require('./lib/RecordStore.js'),
 
-	test = (function(){
-		console.log( RecordStore );
-	})(),
-
 	server = dns.createServer(),
-
-	my_domain = 'dyn.evens.eu',
-
 	records = RecordStore.open( 'mysql-store', 'mysql', 'mysql://dev:dev@localhost/dyndns' );
-	records.addRecord( 'status.dyn.evens.eu', 'A', new dns.A({
-		name: 'status',
-		address: '159.253.3.124',
-		ttl: 300
-	}) );
 
 server.on( 'request', function( request, response ) {
 	var question = request.question[0],
@@ -40,5 +28,4 @@ server.on( 'request', function( request, response ) {
 server.serve( 1053 );
 
 // TODO: Write a REST API to add / delete
-// DONE: Write records to DB, flush DB to disk
 // TODO: Authentication ( per record key? )
