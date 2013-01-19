c381523d51c283d60234bb70c8dc17ce35ce1e47
Jorgen Evens <jorgen@evens.eu>
2013-01-19 22:10:35 +0100

    Updated database scheme for authentication.

--- db.sql
+++ db.sql
@@ -1,8 +1,24 @@
-CREATE TABLE IF NOT EXISTS `records` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(63) NOT NULL,
-  `type` varchar(5) NOT NULL,
-  `data` blob NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY (`domain`,`name`,`type`)
+CREATE TABLE records (
+	id				BIGINT(20)		NOT NULL	AUTO_INCREMENT,
+	name			VARCHAR(63)		NOT NULL,
+	type			VARCHAR(5)		NOT NULL,
+	data			BLOB 			NOT NULL,
+
+	PRIMARY KEY( id ),
+	UNIQUE KEY( domain, name, type ),
+	UNIQUE KEY( update_has )
+);
+
+CREATE TABLE users (
+	id				BIGINT(20)		NOT NULL	AUTO_INCREMENT,
+	username		VARCHAR(50)		NOT NULL,
+	password		VARCHAR(32)		NOT NULL,
+	PRIMARY KEY( id )
+);
+
+CREATE TABLE user_records (
+	user_id			BIGINT(20)		NOT NULL,
+	record_id		BIGINT(20)		NOT NULL,
+	PRIMARY KEY( user_id ),
+	UNIQUE KEY( user_id, record_id )
 );
